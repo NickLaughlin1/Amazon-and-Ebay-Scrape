383a776fa2a2d8d0e8fb5a0ef030238176960f98
Nick Laughlin <nlaughlin@ufl.edu>
2020-05-08 16:26:27 -0400

    can now search for both amazon and ebay

--- Scrapper/AmazonScrape/AmazonScrape/GetUrl.py
+++ Scrapper/AmazonScrape/AmazonScrape/GetUrl.py
@@ -4,21 +4,26 @@ from selenium.webdriver.remote.webdriver import WebDriver
 import time
 
 
-def Find_URL():
+def Find_URL(name):
     # Get what the user wants to search
-    result = input("What would you like to search for on Amazon?" + "\n")
-
-    chromedriver = "C:\\Users\\laugh\\Documents\\PythonDriver\\chromedriver.exe"
+    chromedriver = "Wherever your chromedrive is located"
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
     driver = webdriver.Chrome(chromedriver, options=options)
-    driver.get('https://www.amazon.com/')
-    search = driver.find_element_by_id("twotabsearchtextbox")
+    if(name == "amazon"):
+        result = input("What would you like to search for on Amazon?" + "\n")
+        driver.get('https://www.amazon.com/')
+        search = driver.find_element_by_id("twotabsearchtextbox")
+        # Get the result and search for
+        search.send_keys(result, Keys.ENTER)
+        #time.sleep(30)
+        search = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/span[8]/div/div/span/div/div/ul/li[2]/a').get_attribute('href')
+        driver.close()
+    else:
+        result = input("What would you like to search for on Ebay?" + "\n")
+        driver.get('https://www.ebay.com')
 
-    # Get the result and search for
-    search.send_keys(result, Keys.ENTER)
-    #time.sleep(30)
-    search = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/span[8]/div/div/span/div/div/ul/li[2]/a').get_attribute('href')
-    driver.close()
+        search.send_keys(result, Keys.ENTER)
+        search = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[1]/div/span[8]/div/div/span/div/div/ul/li[2]/a').get_attribute('href')
     return search
     
